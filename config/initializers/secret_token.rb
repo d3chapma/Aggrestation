4a6e5cdc315428ab3806e0a65665ff9d1c7ff58b
David Chapman <talk2dc@gmail.com>
2012-01-10 16:37:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aggrestation::Application.config.secret_token = '7fb3d9141406f30eb95ed41664c8be177616658a1c968071575deb3b50180f6b4a33bf572ed591a213693875b86470bbd715c27107546c036ecd163b66b4ed79'
